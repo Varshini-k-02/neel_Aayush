cfb55017d4fc5193ac7366079c0db7155641b581
Varshini K <78270481+Varshini-k-02@users.noreply.github.com>
2022-09-16 20:52:38 +0530

    Delete base directory

--- base/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Complaint, Comment, ProgressBar
-# Register your models here.
-from .models import User
-admin.site.register(User)
-admin.site.register(Complaint)
-admin.site.register(Comment)
-
-admin.site.register(ProgressBar)
